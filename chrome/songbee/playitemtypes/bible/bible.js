5162a87d3fa916d4e50c273d84c7e4c829d85436
Simon Cozens <simon@simon-cozens.org>
2009-01-30 10:15:13 +0000

    Use our own SQLite bibles instead of the web service.

--- chrome/songbee/playitemtypes/bible/bible.js
+++ chrome/songbee/playitemtypes/bible/bible.js
@@ -1,3 +1,6 @@
+var jsText = readJSFile("playitemtypes/bible/bible_ref_parsing.js");
+eval(jsText);
+
 ItemTypeTable["bible"] = {
     label: "Bible passage",
     create: function () {
@@ -18,16 +21,50 @@ ItemTypeTable["bible"] = {
             this.data(JSON.stringify(obj));
         }
         var frag = doc.createElement("div"); 
+        frag.setAttribute("class", "esv-text");
         frag.innerHTML = obj.passageText;
         return frag;
     }
 };
 
-function getPassage(reference) {
-    var url = "http://www.gnpcb.org/esv/share/get/?key=IP&passage="+escape(reference)+"&action=doPassageQuery";
-    var myXMLHTTPRequest = new XMLHttpRequest();
-    myXMLHTTPRequest.open("GET", url, false);
-    myXMLHTTPRequest.overrideMimeType('text/xml'); // Harder
-    myXMLHTTPRequest.send(null);
-    return myXMLHTTPRequest.responseText;
+var dserv = Components.classes["@mozilla.org/file/directory_service;1"] 
+                     .getService(Components.interfaces.nsIProperties);
+var storageService = Components.classes["@mozilla.org/storage/service;1"]
+                        .getService(Components.interfaces.mozIStorageService);
+
+function getPassage(ref) {
+    var passage = new Reference(ref);
+    return verseText(passage, "net");
+}
+
+function getHandleToBibleVersion(version) {
+    var bible = dserv.get("resource:app", Components.interfaces.nsIFile);
+    bible.append("chrome"); bible.append("songbee");
+    bible.append("playitemtypes"); bible.append("bible");
+    bible.append(version+".db");
+    //if (!bible.exists()) { alert("We don't have that bible version"); return; }
+    return storageService.openDatabase(bible);
+}
+
+function verseText(reference, version) {
+    var handle = getHandleToBibleVersion(version);
+    if (!handle) return "";
+    var iter = reference.iterator();
+    var sql = "SELECT content FROM bible WHERE book = (?1) AND chapter = (?2) AND verse = (?3)";
+    var statement = handle.createStatement(sql);
+    if (!statement) { alert("Oops"); return }
+    // Slow stupid way for now.
+    var text = " ";
+    var bcv;
+    while (bcv = iter.next()) {
+        statement.bindInt32Parameter(0, bcv.book);
+        statement.bindInt32Parameter(1, bcv.chapter);
+        statement.bindInt32Parameter(2, bcv.verse);
+        while (statement.executeStep()) {
+            text += "<p><span class=\"verse-num\">"+bcv.verse+"</span> ";
+            text += statement.getString(0)+"</p> ";
+        }
+        statement.reset();
+    }
+    return text;
 }
