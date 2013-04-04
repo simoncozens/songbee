7506e0bde36901b3a79f045de0158dc54272e4d8
Simon Cozens <simon@simon-cozens.org>
2013-04-04 13:55:01 +0900

    Remove redundant playitemtypes.

--- chrome/songbee/js/songbee.js
+++ chrome/songbee/js/songbee.js
@@ -1,9 +1,30 @@
 var Songbee = {
-
   runFixups: function() {
     for (var a in Songbee.ItemTypes) {
         if (Songbee.ItemTypes[a].fixups) Songbee.ItemTypes[a].fixups();
     }
+  },
+
+  checkUpdates: function () {
+    var updateListener = {
+      onProgress: function(request, position, totalSize) {},
+      onCheckComplete: function(request, updates, updateCount) {
+        if (updateCount > 0) {
+          var prompt = Components.classes["@mozilla.org/updates/update-prompt;1"]. 
+          createInstance(Components.interfaces.nsIUpdatePrompt);
+          prompt.checkForUpdates();
+        }
+      },
+      onError: function(request, update) { alert('update check error'); },
+      QueryInterface: function(aIID) {
+        if (!aIID.equals(Components.interfaces.nsIUpdateCheckListener) && !
+          aIID.equals(Components.interfaces.nsISupports))
+          throw Components.results.NS_ERROR_NO_INTERFACE;
+        return this;
+      }
+    };
+    var checker = Components.classes["@mozilla.org/updates/update-checker;1"].createInstance(Components.interfaces.nsIUpdateChecker);
+    checker.checkForUpdates(updateListener, true);
   },
 
   Utilities: { /* Bootstrapping */
@@ -28,3 +49,4 @@ Songbee.ItemTypes = {};
 supportedTypes.forEach(function(type) {
   Songbee.Utilities.loadJSFile("playitemtypes/"+type+"/"+type+".js", this);
 });
+
